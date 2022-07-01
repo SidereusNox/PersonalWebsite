c95ee3c8347640d51c2c4eac3df6e0cb408bc17d
Philipp Bolliger <philbo96@gmail.com>
2022-07-02 00:50:36 +0200

    added routing

--- Pbolliger/src/app/app-routing.module.ts
+++ Pbolliger/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppWelcomeComponent } from './app-welcome/app-welcome.component';
+import { EducationCardComponent } from './education-card/education-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "welcome", component: AppWelcomeComponent},
+  { path: "about", component: EducationCardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
