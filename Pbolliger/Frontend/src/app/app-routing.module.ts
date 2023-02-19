b92fe5e7fb0fde4fa990e5a2eefd87ba567dd053
Philipp Bolliger <philbo96@gmail.com>
2023-02-19 17:15:32 +0100

    Added Material Theme

--- Pbolliger/Frontend/src/app/app-routing.module.ts
+++ Pbolliger/Frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AppWelcomeComponent } from './app-welcome/app-welcome.component';
 import { EducationCardComponent } from './education-card/education-card.component';
 
 const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: 'full'},
   { path: "home", component: AppWelcomeComponent },
   { path: "about", component: EducationCardComponent },
 ];
