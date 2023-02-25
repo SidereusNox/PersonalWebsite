dce90c220bc4337b976d5da5fd2686ee2a6f53d4
Philipp Bolliger <philbo96@gmail.com>
2023-02-25 16:14:42 +0100

    fix build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppWelcomeComponent } from './app-welcome/app-welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AppWelcomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { AppWelcomeComponent } from './app-welcome/app-welcome.component';
+import { EducationCardComponent } from './education-card/education-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent,
+    AppWelcomeComponent,
+    EducationCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
     BrowserAnimationsModule
   ],
   providers: [],
